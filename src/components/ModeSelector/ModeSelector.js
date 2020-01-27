cd8d638143fe678ef779d2053ec4a51a9480ab7f
Yukiiro-Nite <e.kazuma@gmail.com>
2020-01-26 21:44:35 -0500

    Picked a color palette and grouped selection buttons together

--- src/components/ModeSelector/ModeSelector.js
+++ src/components/ModeSelector/ModeSelector.js
@@ -2,24 +2,28 @@ import React from 'react'
 import './ModeSelector.css'
 
 const ModeSelector = ({onSubmit}) => {
-  return <section className="ModeSelector" onSubmit={onSubmit}>
-    <form className="ModeSelector__form">
-      <input type="hidden" name="mode" value="manage_customers" />
-      <button>Manage Customers</button>
-    </form>
-    <form className="ModeSelector__form">
-      <input type="hidden" name="mode" value="manage_deliveries" />
-      <button>Manage Deliveries</button>
-    </form>
-    <form className="ModeSelector__form">
-      <input type="hidden" name="mode" value="manage_routes" />
-      <button>Manage Routes</button>
-    </form>
-    <form className="ModeSelector__form">
-      <input type="hidden" name="mode" value="monitor_routes" />
-      <button>Monitor Routes</button>
-    </form>
-  </section>
+  return (
+    <div className="ModeSelector__wrapper">
+      <section className="ModeSelector" onSubmit={onSubmit}>
+        <form className="ModeSelector__form">
+          <input type="hidden" name="mode" value="manage_customers" />
+          <button>Manage Customers</button>
+        </form>
+        <form className="ModeSelector__form">
+          <input type="hidden" name="mode" value="manage_deliveries" />
+          <button>Manage Deliveries</button>
+        </form>
+        <form className="ModeSelector__form">
+          <input type="hidden" name="mode" value="manage_routes" />
+          <button>Manage Routes</button>
+        </form>
+        <form className="ModeSelector__form">
+          <input type="hidden" name="mode" value="monitor_routes" />
+          <button>Monitor Routes</button>
+        </form>
+      </section>
+    </div>
+  )
 }
 
 export default ModeSelector
--- src/components/RoleSelector/RoleSelector.js
+++ src/components/RoleSelector/RoleSelector.js
@@ -2,16 +2,20 @@ import React from 'react'
 import './RoleSelector.css'
 
 const RoleSelector = ({onSubmit}) => {
-  return <section className="RoleSelector" onSubmit={onSubmit}>
-    <form className="RoleSelector__form">
-      <input type="hidden" name="role" value="manager" />
-      <button>Delivery Manager</button>
-    </form>
-    <form className="RoleSelector__form">
-      <input type="hidden" name="role" value="deliverer" />
-      <button>Deliverer</button>
-    </form>
-  </section>
+  return (
+    <div className="RoleSelector__wrapper">
+      <section className="RoleSelector" onSubmit={onSubmit}>
+        <form className="RoleSelector__form">
+          <input type="hidden" name="role" value="manager" />
+          <button>Delivery Manager</button>
+        </form>
+        <form className="RoleSelector__form">
+          <input type="hidden" name="role" value="deliverer" />
+          <button>Deliverer</button>
+        </form>
+      </section>
+    </div>
+  )
 }
 
 export default RoleSelector
